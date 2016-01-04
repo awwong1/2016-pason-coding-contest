a2e7c0e9709b2bb47885fbcd513291cf2dd9501a
romansky <sk.romansky@gmail.com>
2016-01-03 22:06:13 -0700

    added the naive pathing back in. In case the other partial pathing was breaking the connection between games.

--- python/algorithm.py
+++ python/algorithm.py
@@ -191,10 +191,12 @@ class Algorithm:
             """
             dist, tank = my_tank.get_closest_dist_tank(enemy_player.tanks)
             tur_dir, tur_rad = my_tank.get_direction_rotation_turret_to_tank(tank)
-
+            tra_dir, tra_rad = my_tank.get_direction_rotation_track_to_tank(tank)
+            
             self.map.get_path(my_tank, tank)
             actions.append(Command.get_turret_rotation_command(my_tank.id, tur_dir, tur_rad, self.client_token))
-            tra_dir, tra_rad, dist = self.generate_tank_path(my_tank, tank, dist)
+            #tra_dir, tra_rad, dist = self.generate_tank_path(my_tank, tank, dist) # use naive method until the pathing works
+
             actions.append(Command.get_tank_rotation_command(my_tank.id, tra_dir, tra_rad, self.client_token))
             # todo don't run over ally
             actions.append(Command.get_movement_command(my_tank.id, 'FWD', dist, self.client_token))
